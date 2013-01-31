f5308a4c7ca1e1c334513ca771608ed7f0e0b60d
Tomas Bulva <tbulva@tomas-bulva-macpro.local>
2013-01-31 09:44:59 -0600

    test commit

--- js/categories_bulk_common.js
+++ js/categories_bulk_common.js
@@ -1,4 +1,6 @@
   jQuery(document).ready(function(){
+  
+  // -- test 
     
     jQuery('form#posts-filter').submit(function(){
         var is_cbec_action = jQuery('form#posts-filter select[name=action]').val();
